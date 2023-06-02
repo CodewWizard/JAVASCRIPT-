23dc224f979649fc3fa42ef3a2ab3b9d3e86f5da
Madiha Mujawar <97935861+CodewWizard@users.noreply.github.com>
2023-06-02 13:04:43 +0530

    Update tut5.js

--- PRACTICE FILES/tut5.js
+++ PRACTICE FILES/tut5.js
@@ -1,20 +1,20 @@
 // String Function
 
 const name = `Madiha`;
-const baby = `Harsh`;
-console.log(name + ` ` + baby);
+const demo = `Harsh`;
+console.log(name + ` ` + demo);
 
 let html;
 let str = "From Madiha";
-html = "<h1> I Lobu Baby </h1>"+
-      "<h1> I Hate You Baby</h1>";
+html = "<h1> demo1 </h1>"+
+      "<h1> demo2</h1>";
 html = html.concat(str);
 console.log(html);
 console.log(html.length);
 console.log(html.toLowerCase());
 console.log(html.toUpperCase());
 console.log(html[7]);
-console.log(html.indexOf(`Baby`));//it is case sensitive;
+console.log(html.indexOf(`demo`));//it is case sensitive;
 console.log(html.lastIndexOf(`a`));
 console.log(html.charAt(5));
 console.log(html.endsWith(str));
@@ -26,7 +26,7 @@ console.log(html.split(' '));
 console.log(html.split('a'));
 console.log(html.replace(`Madiha`, `Maddy`));
 
-let myHtml = `Hello ${name} and ${baby}
+let myHtml = `Hello ${name} and ${demo}
               <h1>Heading</h1>`;
 
 document.body.innerHTML = myHtml;              
